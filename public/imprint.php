4a099b30aa16a589f35be37ed53d0e61996de9f2
Jay <j.trees@hybridsupply.de>
2020-07-22 09:43:20 +0200

    Spam Schutz für E-Mail scraper
    - Email addresse für bots verbergen
    - Echte adresse mit base64 kodieren und beim anklicken decodieren und als mailto verlinken
    - Fallback für deaktivertes JS
    - Verlinkung der addresse von https://www.robinwieschendorf.de auf info@module-loader.de korrigiert

--- public/imprint.php
+++ public/imprint.php
@@ -6,13 +6,15 @@
 
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <link rel="stylesheet" href="/css/style.css">
+
+        <script defer src="/js/default.js"></script>
     </head>
     <body>
         <div class="container">
             <h1>MMLC - Modified Module Loader Client</h1>
 
             <a href="/">zurück zur Startseite</a>
-            
+
             <h2>Impressum</h2>
             <p>
                 <i>Angaben gemäß § 5 TMG und § 55 RStV:</i><br>
@@ -21,15 +23,15 @@
                 Sagehorner Dorfstraße 35b<br>
                 28876 Oyten<br>
                 Telefon: 04207 9998787<br>
-                E-Mail: mail@robinwieschendorf.de<br>
+                E-Mail: <a class="electronicMessage" data-href="bWFpbEByb2JpbndpZXNjaGVuZG9yZi5kZQ==" tabindex="0">mail<span class="at"></span>robinwieschendorf<span class="dot"></span>de</a><br>
                 Web: <a href="https://www.robinwieschendorf.de">www.robinwieschendorf.de</a><br>
-                
+
 
                 <div class="notice info">
-                    <strong>Info:</strong> Fragen zum MMLC bitte an <a target="_blank" href="https://discord.gg/9NqwJqP">Discord #mmlc DE/EN</a> oder <a href="https://www.robinwieschendorf.de">info@module-loader.de</a>
+                    <strong>Info:</strong> Fragen zum MMLC bitte an <a target="_blank" href="https://discord.gg/9NqwJqP">Discord #mmlc DE/EN</a> oder <a class="electronicMessage" data-href="aW5mb0Btb2R1bGUtbG9hZGVyLmRl" tabindex="0">info<span class="at"></span>module-loader<span class="dot"></span>de</a>
                 </div>
 
-                
+
             </p>
         </div>
 
