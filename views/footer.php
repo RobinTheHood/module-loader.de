7a1a748ca9b31949afd3b98ca636e66d6b9235ba
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-09-07 20:25:12 +0200

    style: add github image at footer

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,11 @@
 <footer>
+
+    <div>
+        <a href="https://github.com/RobinTheHood/ModifiedModuleLoaderClient">
+            <img style="width: 60px" src="/images/GitHub-Mark-120px-plus.png">
+        </a>
+    </div>
+
     <div style="text-align: center">
         Entwickler: <a href="https://www.robinwieschendorf.de">Robin Wieschendorf</a>
     </div>
