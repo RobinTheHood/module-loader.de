8e62708cf758fe3be3a86d61da974f3a02d8def1
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-09-08 09:45:50 +0200

    docs: add beta_info.php

---
+++ public/inc/beta_info.php
@@ -0,0 +1,5 @@
+<aside>
+    <div class="notice danger">
+        <p><strong>Hinweis:</strong> Der MMLC befindet sich in der <strong>Beta-Testphase</strong>. Diese Webseite bietet dir bereits jetzt erste Einblicke. <strong>Beta-Tester</strong> bekommen die Möglichkeit das System auszuprobieren. Solltest du Fehler entdecken oder dich für den MMLC interessieren, freuen wir uns über eine Nachricht unter <a target="_blank" href="https://discord.gg/9NqwJqP">Discord #mmlc DE/EN</a> oder unter <a class="electronicMessage" data-href="aW5mb0Btb2R1bGUtbG9hZGVyLmRl" tabindex="0">info<span class="at"></span>module-loader<span class="dot"></span>de</a>.</p>
+    </div>
+</aside>
