ab2c36c717c1d2be3949b88e7839f37a17ed76d7
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-09-08 09:46:16 +0200

    docs: add contribute_info.php

---
+++ public/inc/contribute_info.php
@@ -0,0 +1,5 @@
+<aside>
+    <div class="notice info">
+        <p><strong>Hinweis:</strong> Diese Seite ist noch unvollständig. Wenn du Lust hast dich an diesem Projekt zu beteiligen, kannst du auf <a target="blank" href="https://github.com/RobinTheHood/module-loader.de">GitHub</a> ein Fork von dieser Webseite machen und diese Webseite mit einem PullRequest verbessern. Falls du Fragen hast, kannst du dich auf unserem <a target="_blank" href="https://discord.gg/9NqwJqP">Discord #mmlc DE/EN</a> Server mit der Community austauschen</a> oder schreibe uns eine E-Mail an <a class="electronicMessage" data-href="aW5mb0Btb2R1bGUtbG9hZGVyLmRl" tabindex="0">info<span class="at"></span>module-loader<span class="dot"></span>de</a>.</p>
+    </div>
+</aside>
