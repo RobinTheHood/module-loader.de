b46034fc06dd3e3181b2d382b70f7d51c697e99a
Jay <j.trees@hybridsupply.de>
2020-09-22 15:51:02 +0200

    fix: privacy and imprint links not working

--- public/imprint.php
+++ public/imprint.php
@@ -8,37 +8,35 @@
         <title>Impressum - MMLC - Modified Module Loader Client</title>
     </head>
     <body>
-        <div class="pageWrapper">
-            <header>
+        <header>
+            <nav>
                 <?php
-                require_once 'inc/pageTitle.php';
+                require_once 'inc/navigation.php';
                 ?>
+            </nav>
+        </header>
 
-                <nav>
-                    <?php
-                    require_once 'inc/navigation.php';
-                    ?>
-                </nav>
-            </header>
+        <div class="pageWrapper">
+            <div class="content">
+                <main>
+                    <section>
+                      <div class="notice info">
+                          <p><strong>Info:</strong> Fragen zum MMLC bitte an <a target="_blank" href="https://discord.gg/9NqwJqP">Discord #mmlc DE/EN</a> oder <a class="electronicMessage" data-href="aW5mb0Btb2R1bGUtbG9hZGVyLmRl" tabindex="0">info<span class="at"></span>module-loader<span class="dot"></span>de</a></p>
+                      </div>
 
-            <main>
-                <section>
-                  <div class="notice info">
-                      <p><strong>Info:</strong> Fragen zum MMLC bitte an <a target="_blank" href="https://discord.gg/9NqwJqP">Discord #mmlc DE/EN</a> oder <a class="electronicMessage" data-href="aW5mb0Btb2R1bGUtbG9hZGVyLmRl" tabindex="0">info<span class="at"></span>module-loader<span class="dot"></span>de</a></p>
-                  </div>
-
-                  <h2>Impressum</h2>
-                  <p>Angaben gemäß § 5 TMG und § 55 RStV:<p>
-                  <p>
-                      Robin Wieschendorf<br>
-                      Sagehorner Dorfstraße 35b<br>
-                      28876 Oyten<br>
-                      Telefon: 04207 9998787<br>
-                      E-Mail: <a class="electronicMessage" data-href="bWFpbEByb2JpbndpZXNjaGVuZG9yZi5kZQ==" tabindex="0">mail<span class="at"></span>robinwieschendorf<span class="dot"></span>de</a><br>
-                      Web: <a href="https://www.robinwieschendorf.de">www.robinwieschendorf.de</a>
-                  </p>
-                </section>
-            </main>
+                      <h2>Impressum</h2>
+                      <p>Angaben gemäß § 5 TMG und § 55 RStV:<p>
+                      <p>
+                          Robin Wieschendorf<br>
+                          Sagehorner Dorfstraße 35b<br>
+                          28876 Oyten<br>
+                          Telefon: 04207 9998787<br>
+                          E-Mail: <a class="electronicMessage" data-href="bWFpbEByb2JpbndpZXNjaGVuZG9yZi5kZQ==" tabindex="0">mail<span class="at"></span>robinwieschendorf<span class="dot"></span>de</a><br>
+                          Web: <a href="https://www.robinwieschendorf.de">www.robinwieschendorf.de</a>
+                      </p>
+                    </section>
+                </main>
+            </div>
         </div>
 
         <?php include '../views/footer.php'; ?>
