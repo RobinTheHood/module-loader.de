4568abffcf637cb4e6224cbac7c71213dccb2430
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-11-03 14:22:45 +0100

    feat: add og:image

--- views/head.php
+++ views/head.php
@@ -2,6 +2,20 @@
 <meta name="author" content="Robin Wieschendorf">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://module-loader.de/">
+<meta property="og:type" content="website">
+<meta property="og:title" content="MMLC - Modified Module Loader Client">
+<meta property="og:description" content="Der MMLC ist eine Software zur Verwaltung von Modulen in deinem modified eCommerce Shop.">
+<meta property="og:image" content="https://module-loader.de/images/Modified-Shop-Module-Verwaltung-MMLC.jpg">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="module-loader.de">
+<meta property="twitter:url" content="https://module-loader.de/">
+<meta name="twitter:title" content="MMLC - Modified Module Loader Client">
+<meta name="twitter:description" content="Der MMLC ist eine Software zur Verwaltung von Modulen in deinem modified eCommerce Shop.">
+<meta name="twitter:image" content="https://module-loader.de/images/Modified-Shop-Module-Verwaltung-MMLC.jpg">
 
 <!-- Favicons / App Icons -->
 <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-touch-icon.png">
