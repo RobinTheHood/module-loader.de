cf464d342be1eefd3657d213919a97ed605907d5
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-11-03 14:22:11 +0100

    docs: add facions

--- views/head.php
+++ views/head.php
@@ -2,6 +2,16 @@
 <meta name="author" content="Robin Wieschendorf">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 
+
+<!-- Favicons / App Icons -->
+<link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<!-- <link rel="mask-icon" href="/images/icons/safari-pinned-tab.svg" color="#2A71F9"> -->
+<meta name="msapplication-TileColor" content="#00aba9">
+<meta name="theme-color" content="#ffffff">
+
 <link rel="stylesheet" href="/css/font-awesome-5-14-0.css">
 <link rel="stylesheet" href="/css/fonts.css">
 
